0518d581ca4394fb65ed6cef5ab8cfd6431cefd2
sachinft <sachin11061998@gmail.com>
2022-12-19 00:34:01 +0530

    hi

--- pages/api/moralis/[...moralis].ts
+++ pages/api/moralis/[...moralis].ts
@@ -3,7 +3,7 @@ import { MoralisNextApi } from '@moralisweb3/next';
 export default MoralisNextApi({
   apiKey: process.env.MORALIS_API_KEY || '',
   authentication: {
-    domain: 'https://eth-boilerplate.vercel.app/',
+    domain: 'ethereum-boilerplate.dapp',
     uri: process.env.NEXTAUTH_URL || '',
     timeout: 120,
   },
