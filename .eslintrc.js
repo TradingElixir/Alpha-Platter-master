97389ad3cdfed80d5d608a21d767a0ff89d813c2
sachinft <sachin11061998@gmail.com>
2022-12-19 09:18:09 +0530

    hi

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,14 @@
-module.exports = {
+(module.exports = {
   extends: ['@moralisweb3', 'plugin:@next/next/recommended'],
   ignorePatterns: ['**/build/**/*'],
   rules: {
     'no-console': 'off',
   },
-};
+}),
+  {
+    extends: 'next',
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  };
